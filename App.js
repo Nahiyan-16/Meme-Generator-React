40572c481fb588a5707c51f3de97d666678d88ff
Nahiyan Ahmed <nahiyanahmed11@gmail.com>
2022-12-22 14:41:34 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from "react"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 
-export default function Header() {
+export default function App() {
     return (
-        <header className="header">
-            <img 
-                src="./images/troll-face.png" 
-                className="header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
-        </header>
+        <div>
+            <Header />
+            <Meme />
+        </div>
     )
 }
+
